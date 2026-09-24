f6e0eccef7de278504b10d092fbc78027adadc00
Thierry Kauãn da Silva <art0073299@aluno.firjansenaisesi.com.br>
2026-09-24 14:55:39 -0300

    Adicionando Grid de Tecnologias' a git '' ' clear

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ mensagem.textContent =
  
 formulario.reset();
 });
+
+const texto = [
+"Desenvolvedor Front-End",
+"Estudante de Programação",
+"Criador de Interfaces Modernas",
+"Apaixonado por Tecnologia"
+];
+ 
+let textoIndex = 0;
+let caractereIndex = 0;
+ 
+const elemento = document.getElementById("typing");
+ 
+function digitar() {
+if (caractereIndex < texto[textoIndex].length) {
+elemento.textContent += texto[textoIndex].charAt(caractereIndex);
+caractereIndex++;
+ 
+setTimeout(digitar, 80);
+} else {
+setTimeout(apagar, 2000);
+}
+}
+ 
+function apagar() {
+if (caractereIndex > 0) {
+elemento.textContent =
+texto[textoIndex].substring(0, caractereIndex - 1);
+ 
+caractereIndex--;
+ 
+setTimeout(apagar, 40);
+} else {
+textoIndex++;
+ 
+if (textoIndex >= texto.length) {
+textoIndex = 0;
+}
+ 
+setTimeout(digitar, 300);
+}
+}
+ 
+digitar();
